40ef21f7c75126dd78f710ba58623968987675c8
La-Ruda <herempen@gmail.com>
2023-05-19 19:25:20 +0200

    mrdat

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 console.log('funguju!');
 
+const movieList = () => {
+
+    fetch("https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies").then(
+        (movies) => {
+            return movies.json();
+        })
+        .then((movies) => {
+            console.log(movies)
+        });
+};
 
 
-console.log("skrrrr");
+
+let kunda = "3";
+
+
+let mrdej = document.querySelector(".mrdej").addEventListener("click", movieList);
+
+
+
+
+
